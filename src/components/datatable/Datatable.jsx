9c43ddf17867359f1963eaf5447d64f465795d94
mirzasalim <111202113671@mhs.dinus.ac.id>
2024-06-20 10:04:54 +0700

    coba

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,54 +1,64 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Salim", firstName: "Mirza", age: 21 },
-  { id: 2, lastName: "Safitro", firstName: "Ilham", age: 19 },
-  { id: 3, lastName: "Arya", firstName: "Kevin", age: 20 },
-  { id: 4, lastName: "Priyo", firstName: "Alvin", age: 16 },
-  { id: 5, lastName: "Alfatir", firstName: "Dimas", age: null },
-  { id: 6, lastName: "Meli", firstName: null, age: 53 },
-  { id: 7, lastName: "Ferdiansyah", firstName: "Farid", age: 44 },
-  { id: 8, lastName: "Valen", firstName: "Rossi", age: 36 },
-  { id: 9, lastName: "Rivandi", firstName: "Septyan", age: 65 },
-];
-
-const Datatable = () => {
+const Datatable = ({columns}) => {
   const location = useLocation();
   const type = location.pathname.split('/')[1];
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+  const unsub = onSnapshot(
+    collection(db, type),
+    (snapShot) => {
+      let list = [];
+      snapShot.docs.forEach((doc) => {
+        list.push({ id: doc.id, ...doc.data() });
+      });
+      setData(list);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
+  return () => {
+    unsub();
+  };
+}, [type]); 
+
+const handleDelete = async (id) => {
+  try {
+    await deleteDoc(doc(db, type, id));
+    setData(data.filter((item) => item.id !== id));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const actionColumn = [
   {
     field: "action",
     headerName: "Action",
     width: 200,
-    renderCell: () => {
+    renderCell: (params) => {
       return (
         <div className="cellAction">
-          <Link to={"/" + type + "/test"} style={{ textDecoration: "none" }}>
+          <Link to={"/" + type + "/" + params.row.id} style={{ textDecoration: "none" }}>
             <span className="viewButton">View</span>
           </Link>
+          <span>
+	          <span
+	            className="deleteButton"
+	            onClick={() => handleDelete(params.row.id)}
+	          >
+	            Delete
+	          </span>
+	        </span>
         </div>
       );
     },
@@ -64,7 +74,7 @@ const actionColumn = [
         </Link>
       </div>
       <DataGrid className="datagrid" 
-        rows={rows}
+        rows={data}
         columns={columns.concat(actionColumn)}
         initialState={{
           pagination: {
